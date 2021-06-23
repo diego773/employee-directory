6a9ceefe58af5920ab02a3ae19b4d59034aaa0a4
Diego <diegovillarreal123@gmail.com>
2021-06-23 14:11:48 -0500

    created SearchResultContainer.js and also added render, class SearchResultContainer, compountDidMount

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Random User API
+const BASEURL = "https://randomuser.me/api/?results=25&nat=us"
+// Export an object containing methods we'll use for accessing the Random Employee API
 export default {
-    getRandomUser: function() {
-        return axios.get("https://randomuser.me/api/?results=25")
-    },
-
-}
+    getRandomEmployee: function() {
+        return axios.get(BASEURL)
+    }
+};
